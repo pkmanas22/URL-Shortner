cdc2dd6d859c7f297d91a67a218a8d5e6bebf02f
Manas Kumar Pradhan <pkmanas22@gmail.com>
2024-04-26 05:04:26 +0530

    mongoose solved

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,26 +1,29 @@
 const { getUser } = require('../services/jwtAuth');
 
 function checkForAuthentication(req, res, next) {
-    const tokenCookie = req.cookies?.uid;
-    const sessionCookie = req.cookies?.s_uid;
 
-    req.user = null;
+    if (req.cookies) {
+        const tokenCookie = req.cookies.uid;
+        const sessionCookie = req.cookies.s_uid;
 
-    try {
-        if (tokenCookie) {
-            const user = getUser(tokenCookie);
-            req.user = user;
-            return next();
-        } else if (sessionCookie) {
-            const user = getUser(sessionCookie);
-            req.user = user;
-            return next();
-        } else {
-            return res.redirect("/trial");
+        req.user = null;
+
+        try {
+            if (tokenCookie) {
+                const user = getUser(tokenCookie);
+                req.user = user;
+                return next();
+            } else if (sessionCookie) {
+                const user = getUser(sessionCookie);
+                req.user = user;
+                return next();
+            } else {
+                return res.redirect("/trial");
+            }
+        } catch (error) {
+            console.error("Error while decoding JWT:", error);
+            return res.status(500).send("Internal Server Error");
         }
-    } catch (error) {
-        console.error("Error while decoding JWT:", error);
-        return res.status(500).send("Internal Server Error");
     }
 }
 
